49fefc380379a84e01962d8f77ec93e9f3bdf49c
Joao Hencke <joao.hencke@gmail.com>
2019-04-16 19:39:30 -0300

    refactor on how to use authentication on router  - avoiding code duplication

--- server/booking/router/booking.read.js
+++ server/booking/router/booking.read.js
@@ -8,22 +8,18 @@ const readRouter = express.Router({ mergeParams: true });
 
 module.exports = readRouter;
 
-readRouter.get(
-  '/',
-  auth.authenticate(),
-  validate({ page: 'numeric?', limit: 'numeric?' }, 'query'),
-  (req, res, next) => {
-    bookingManager
-      .list(req.validData)
-      .then(result => res.json(result))
-      .catch(next);
-  },
-);
+readRouter.use(auth.authenticate());
+
+readRouter.get('/', validate({ page: 'numeric?', limit: 'numeric?' }, 'query'), (req, res, next) => {
+  bookingManager
+    .list(req.validData)
+    .then(result => res.json(result))
+    .catch(next);
+});
 
 readRouter.get(
   '/:id',
 
-  auth.authenticate(),
   validate({ id: 'string' }, 'params'),
   (req, res, next) => {
     bookingManager
