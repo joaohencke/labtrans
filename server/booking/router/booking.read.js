8dc943cc92355973da871604255d6a1299650d61
Joao Hencke <joao.hencke@gmail.com>
2019-04-16 19:34:26 -0300

    booking read api - list and get by id

--- server/booking/router/booking.read.js
+++ server/booking/router/booking.read.js
@@ -1,5 +1,34 @@
 const express = require('express');
 
+const { validate } = require('../../utils/validation');
+const auth = require('../../auth');
+const bookingManager = require('../');
+
 const readRouter = express.Router({ mergeParams: true });
 
 module.exports = readRouter;
+
+readRouter.get(
+  '/',
+  auth.authenticate(),
+  validate({ page: 'numeric?', limit: 'numeric?' }, 'query'),
+  (req, res, next) => {
+    bookingManager
+      .list(req.validData)
+      .then(result => res.json(result))
+      .catch(next);
+  },
+);
+
+readRouter.get(
+  '/:id',
+
+  auth.authenticate(),
+  validate({ id: 'string' }, 'params'),
+  (req, res, next) => {
+    bookingManager
+      .get(req.validData)
+      .then(result => res.json(result))
+      .catch(next);
+  },
+);
