63839de6bfbb23b59ff4dbae82ade69452017400
Joao Hencke <joao.hencke@gmail.com>
2019-04-16 19:55:09 -0300

    create/delete/update booking routes

--- server/booking/router/booking.write.js
+++ server/booking/router/booking.write.js
@@ -1,5 +1,42 @@
 const express = require('express');
 
+const auth = require('../../auth');
+const { validate } = require('../../utils/validation');
+const bookingManager = require('../');
+
 const writeRouter = express.Router({ mergeParams: true });
 
 module.exports = writeRouter;
+
+writeRouter.use(auth.authenticate());
+
+const defaultBody = {
+  description: 'string',
+  beginTs: 'numeric',
+  endTs: 'numeric',
+  responsible: 'string',
+  place: 'string',
+  room: 'string',
+  people: 'numeric?',
+};
+
+writeRouter.post('/', validate(defaultBody, 'body'), (req, res, next) => {
+  bookingManager
+    .create(req.validData)
+    .then(result => res.status(201).json(result))
+    .catch(next);
+});
+
+writeRouter.put('/:id', validate({ ...defaultBody, id: 'string' }), (req, res, next) => {
+  bookingManager
+    .update(req.validData)
+    .then(result => res.json(result))
+    .catch(next);
+});
+
+writeRouter.delete('/:id', validate({ id: 'string' }, 'params'), (req, res, next) => {
+  bookingManager
+    .remove(req.validData)
+    .then(() => res.status(204).end())
+    .catch(next);
+});
