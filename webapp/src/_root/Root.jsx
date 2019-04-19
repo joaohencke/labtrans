a2618d240700d139800e4d11958d3d6cb8aec93e
Joao Hencke <joao.hencke@gmail.com>
2019-04-19 15:31:01 -0300

    refresh token page reload

--- webapp/src/_root/Root.jsx
+++ webapp/src/_root/Root.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { renderRoutes } from "react-router-config";
 import { Redirect } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { connect } from 'react-redux';
-import { authorization } from '../utils/http';
+import { connect } from "react-redux";
+import { authorization } from "../utils/http";
 
-const Root = ({ route, location, logged, credential}) => {
-  if (credential) authorization(`${credential.token_type} ${credential.access_token}`);
-  
+const Root = ({ route, location, logged, credential }) => {
+  if (credential)
+    authorization(`${credential.token_type} ${credential.access_token}`);
+
   if (!logged && location.pathname !== "/login")
     return <Redirect to="/login" />;
 
@@ -18,8 +19,5 @@ const mapStateToProps = state => ({
   logged: state.auth.logged,
   credential: state.auth.credential
 });
-
-
-
 
 export default connect(mapStateToProps)(Root);
