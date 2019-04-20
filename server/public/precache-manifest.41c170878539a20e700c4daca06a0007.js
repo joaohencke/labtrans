81c2c42e535ec53debd008a8060c2eef4267808d
Joao Hencke <joao.hencke@gmail.com>
2019-04-20 13:04:41 -0300

    build version

---
+++ server/public/precache-manifest.41c170878539a20e700c4daca06a0007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fcf8f691019e8dc167e",
+    "url": "/static/js/main.90a83642.chunk.js"
+  },
+  {
+    "revision": "e3e05c48ff2fa9d3f114",
+    "url": "/static/js/2.d5db8fa3.chunk.js"
+  },
+  {
+    "revision": "e3e05c48ff2fa9d3f114",
+    "url": "/static/css/2.047a3f50.chunk.css"
+  },
+  {
+    "revision": "b7663ae3df1d255bc48fe15851fec3f9",
+    "url": "/index.html"
+  }
+];
